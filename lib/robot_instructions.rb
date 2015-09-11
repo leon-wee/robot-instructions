7d48e6c08c5af0ce4a1bf479bcfa0e434927dd79
leon-wee <leonwee27@gmail.com>
2015-09-11 14:10:39 +0700

    Done

--- lib/robot_instructions.rb
+++ lib/robot_instructions.rb
@@ -1,4 +1,25 @@
 class RobotInstructions
 
 
+  def self.call(start:, ended:)
+
+    sx = start[0]
+    sy = start[1]
+    ex = ended[0]
+    ey = ended[1]
+
+    array = []
+    array << [sx,sy]
+
+    while (sx != ex) || (sy != ey) do
+      sx = sx + (ex <=> sx)
+      sy = sy + (ey <=> sy)
+      array << [sx,sy]
+    end
+
+    array
+
+  end
+
+
 end
--- spec/robot_instructions_spec.rb
+++ spec/robot_instructions_spec.rb
@@ -3,19 +3,19 @@ require './lib/robot_instructions'
 describe RobotInstructions do
 
   it 'calculates an incremental route' do
-    expect(RobotInstructions.call(start: [3,5], end: [10,10])).to eq(
+    expect(RobotInstructions.call(start: [3,5], ended: [10,10])).to eq(
        [[3,5],[4,6],[5,7],[6,8],[7,9],[8,10],[9,10],[10,10]]
     )
   end
 
   it 'calculates a decremental route' do
-    expect(RobotInstructions.call(start: [7, 8], end: [3, 2])).to eq(
+    expect(RobotInstructions.call(start: [7, 8], ended: [3, 2])).to eq(
       [[7, 8], [6, 7], [5, 6], [4, 5], [3, 4], [3, 3], [3, 2]]
     )
   end
 
   it 'calculates a route from negative coordinates' do
-    expect(RobotInstructions.call(start: [-2,-3], end: [1,1])).to eq(
+    expect(RobotInstructions.call(start: [-2,-3], ended: [1,1])).to eq(
       [[-2,-3],[-1,-2],[0,-1],[1,0],[1,1]]
     )
   end
